684279f89120eda114da191949200f02d42a9630
GearLuFFy <c-mugiwara@hotmail.com>
2015-10-15 20:20:48 +0300

    Initial commit

---
+++ app/src/main/java/com/example/c_mug/proxshoppingcart/HomeActivity.java
@@ -0,0 +1,82 @@
+package com.example.c_mug.proxshoppingcart;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class HomeActivity extends AppCompatActivity {
+
+    IntentIntegrator integrator = new IntentIntegrator(HomeActivity.this);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        final Button pair = (Button) findViewById(R.id.pair);
+
+        View.OnClickListener Listen = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                integrator.addExtra("SCAN_WIDTH", 640);
+                integrator.addExtra("SCAN_HEIGHT", 480);
+                integrator.addExtra("SCAN_MODE", "QR_CODE_MODE");
+                integrator.initiateScan(IntentIntegrator.QR_CODE_TYPES);
+                integrator.addExtra("SAVE_HISTORY", false);//this stops saving ur barcode in barcode scanner app's history
+                
+
+            }
+        };
+                pair.setOnClickListener(Listen);
+
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        if (result != null) {
+            String contents = result.getContents();
+            if (contents != null) {
+
+                Toast.makeText(this, contents , Toast.LENGTH_LONG).show();
+
+            } else {
+                Toast.makeText(this, "Nothing" , Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
